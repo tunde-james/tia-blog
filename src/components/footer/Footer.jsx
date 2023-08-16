d20e18247deedf69b84a502ed42510cbe60a54c1
Tunde James <tundejames04@gmail.com>
2023-08-16 08:36:10 +0100

    Add the css module file for each page Add the Navbar and Footer styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
+import styles from './footer.module.css';
+import Image from 'next/image';
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className={styles.container}>
+      <div>©2023 Fortitude. All rights reserved</div>
+      <div className={styles.social}>
+        <Image
+          src="/1.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="Tia Dev Facebook"
+        />
+        <Image
+          src="/2.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="Tia Dev Instagram"
+        />
+        <Image
+          src="/3.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="Tia Dev Twitter"
+        />
+        <Image
+          src="/4.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="Tia Dev YouTube"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+'use client';
+
+import Link from 'next/link';
+import React from 'react';
+import styles from './navbar.module.css';
+
+const links = [
+  { id: 1, title: 'Home', url: '/' },
+  { id: 2, title: 'Portfolio', url: '/portfolio' },
+  { id: 3, title: 'Blog', url: '/blog' },
+  { id: 4, title: 'About', url: '/about' },
+  { id: 5, title: 'Contact', url: '/contact' },
+  { id: 1, title: 'Dashboard', url: '/dashboard' },
+];
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className={styles.container}>
+      <Link href="/" className={styles.logo}>
+        fortitude
+      </Link>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <Link key={link.id} href={link.url} className={styles.link}>
+            {link.title}
+          </Link>
+        ))}
+        <button
+          onClick={() => console.log('Logged out')}
+          className={styles.logout}
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
