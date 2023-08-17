6845d5af8abcd01e52d1a24114b0e6562b5ed497
Tunde James <tundejames04@gmail.com>
2023-08-17 12:46:50 +0100

    Work on the portfolio  page content and styles

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import Button from '@/components/button/Button';
+import styles from './category.module.css';
 
-function Category() {
+function Category({ params }) {
+  console.log(params);
+
   return (
-    <div>Category</div>
-  )
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <section className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button url="#">See More</Button>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            fill={true}
+            src="https://images.pexels.com/photos/887822/pexels-photo-887822.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            className={styles.img}
+          />
+        </div>
+      </section>
+      <section className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button url="#">See More</Button>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            fill={true}
+            src="https://images.pexels.com/photos/887822/pexels-photo-887822.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            className={styles.img}
+          />
+        </div>
+      </section>
+      <section className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button url="#">See More</Button>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            fill={true}
+            src="https://images.pexels.com/photos/887822/pexels-photo-887822.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            className={styles.img}
+          />
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default Category
+export default Category;
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import styles from './page.module.css';
 
 function Portfolio() {
   return (
-    <div>Portfolio</div>
-  )
+    <div className={styles.container}>
+      <h1 className={styles.selectTitle}>Choose a gallery</h1>
+      <div className={styles.items}>
+        <Link href="/portfolio/illustrations" className={styles.item}>
+          <span className={styles.title}>illustrations</span>
+        </Link>
+        <Link href="/portfolio/websites" className={styles.item}>
+          <span className={styles.title}>websites</span>
+        </Link>
+        <Link href="/portfolio/applications" className={styles.item}>
+          <span className={styles.title}>applications</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Portfolio
+export default Portfolio;
