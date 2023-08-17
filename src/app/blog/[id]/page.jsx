e68e0e3173a1e09160eab0a3efa3ac01dafd2e7e
Tunde James <tundejames04@gmail.com>
2023-08-17 13:35:48 +0100

    Add the content for the blog page and apply its styles

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,7 +1,51 @@
 import React from 'react';
+import Image from 'next/image';
+import styles from './blogpost.module.css';
 
 function BlogPost() {
-  return <div>BlogPost</div>;
+  return (
+    <section className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>
+            Lorem ipsum dolor sit amet consectetur adipisicing
+          </h1>
+          <p className={styles.desc}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            quo voluptates nobis repellat?
+          </p>
+          <div className={styles.author}>
+            <Image
+              src="https://images.pexels.com/photos/17894573/pexels-photo-17894573/free-photo-of-man-sitting-in-outdoor-wear.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>John Doe</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/206359/pexels-photo-206359.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam quo
+          voluptates nobis repellat? Eius, commodi Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Quisquam quo voluptates nobis repellat?
+          Eius, commodi. Lorem ipsum dolor sit amet consectetur adipisicing.
+          Eius, commodi Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Quisquam quo voluptates nobis repellat? Eius, commodi
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default BlogPost;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,8 +1,60 @@
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 import styles from './page.module.css';
 
 function Blog() {
-  return <div className={styles.container}>Blog</div>;
+  return (
+    <div className={styles.mainContainer}>
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://images.pexels.com/photos/2344582/pexels-photo-2344582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://images.pexels.com/photos/2344582/pexels-photo-2344582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://images.pexels.com/photos/2344582/pexels-photo-2344582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+    </div>
+  );
 }
 
 export default Blog;
